bce4360a74e3c456e406fe6c7b5cc598e7098e7e
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-07-25 12:15:00 -0400

    initial setup

---
+++ main.py
@@ -0,0 +1,29 @@
+from typing import Union
+from pypdf import PdfReader
+import io
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+
+@app.get("/ocr")
+def extract_text(url: Union[str, None] = None):
+    if url is None:
+        return {"error": "Please provide a url"}
+
+    res = requests.get(url)
+    buffer = io.BytesIO(res.content)
+    reader = PdfReader(buffer)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text() + " "
+
+    # remove special characters
+    text = text.replace("\n", " ").replace("\t", " ").replace(
+        "●", " ").replace("■", " ").replace("□", " ").replace("▪", " ")
+
+    # replace multiple spaces with single space
+    text = " ".join(text.split())
+
+    return {"text": text}
